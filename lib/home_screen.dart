0f0f9732787d09c8ecc591155cbc38c47da0263a
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2023-11-01 15:06:03 +0530

    create container

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,9 +11,76 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Text(
+                    "BMI CALCULATOR",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 160,
+                      width: 160,
+                      decoration: BoxDecoration(
+                          color: const Color(0xFF484149),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.male,
+                              color: Colors.white,
+                              size: 100,
+                            ),
+                          ),
+                          Text(
+                            "Male",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 160,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF484149),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.female_outlined,
+                              color: Colors.white,
+                              size: 100,
+                            ),
+                          ),
+                          const Text(
+                            "Female",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+        ),
       ),
     );
   }
