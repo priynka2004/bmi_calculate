4e88901d72470f0f9782cf71447cd52b0bc4f4f0
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2023-11-01 21:24:36 +0530

    create bmi calculation

--- lib/bmi_calculation_screen.dart
+++ lib/bmi_calculation_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+
+class BmiCalculationScreen extends StatefulWidget {
+  const BmiCalculationScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<BmiCalculationScreen> createState() => _BmiCalculationScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  double _currentSliderValue = 60;
+class _BmiCalculationScreenState extends State<BmiCalculationScreen> {
+  double height = 1.80;
+
+  int age = 20;
+  int weight = 40;
+  String result = '';
+
+  void calculateBMI() {
+    double meter = height / 100;
+    double bmi = weight / (meter * meter);
+    result = bmi.toStringAsFixed(2);
+  }
+
+  void incrementAge() {
+    setState(() {
+      age++;
+    });
+  }
+
+  void decrementAge() {
+    if (age > 0) {
+      setState(() {
+        age--;
+      });
+    }
+  }
+
+  void incrementWeight() {
+    setState(() {
+      weight++;
+    });
+  }
+
+  void decrementWeight() {
+    if (weight > 0) {
+      setState(() {
+        weight--;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,30 +56,32 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: const Color(0xFF242225),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.04,
+            right: MediaQuery.of(context).size.width * 0.04,
+          ),
+          child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                const Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: Text(
-                    "BMI Calculate",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 38,
-                        fontWeight: FontWeight.bold),
+                Text(
+                  "BMI CALCULATOR",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.height * 0.04,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      height: 160,
-                      width: 160,
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      width: MediaQuery.of(context).size.width * 0.4,
                       decoration: BoxDecoration(
-                          color: const Color(0xFF484149),
-                          borderRadius: BorderRadius.circular(20)),
+                        color: const Color(0xFF484149),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                       child: Column(
                         children: [
                           IconButton(
@@ -47,19 +89,19 @@ class _HomeScreenState extends State<HomeScreen> {
                             icon: const Icon(
                               Icons.male,
                               color: Colors.white,
-                              size: 100,
+                              size: 80,
                             ),
                           ),
                           const Text(
                             "Male",
                             style: TextStyle(color: Colors.white, fontSize: 30),
-                          )
+                          ),
                         ],
                       ),
                     ),
                     Container(
-                      height: 160,
-                      width: 160,
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      width: MediaQuery.of(context).size.width * 0.4,
                       decoration: BoxDecoration(
                         color: const Color(0xFF484149),
                         borderRadius: BorderRadius.circular(20),
@@ -71,20 +113,17 @@ class _HomeScreenState extends State<HomeScreen> {
                             icon: const Icon(
                               Icons.female_outlined,
                               color: Colors.white,
-                              size: 100,
+                              size: 80,
                             ),
                           ),
                           const Text(
                             "Female",
                             style: TextStyle(color: Colors.white, fontSize: 30),
-                          )
+                          ),
                         ],
                       ),
                     ),
                   ],
-                ),
-                const SizedBox(
-                  height: 16,
                 ),
                 Container(
                   height: 160,
@@ -100,128 +139,82 @@ class _HomeScreenState extends State<HomeScreen> {
                         "HEIGHT",
                         style: TextStyle(color: Colors.white, fontSize: 30),
                       ),
-                      const Row(
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            "180",
-                            style: TextStyle(color: Colors.white, fontSize: 40),
+                            height.toStringAsFixed(2),
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 40),
                           ),
-                          Text(
+                          const Text(
                             "cm",
                             style: TextStyle(color: Colors.white, fontSize: 25),
                           ),
                         ],
                       ),
                       Slider(
-                        activeColor: Colors.white,
-                        thumbColor: Colors.red,
-                        value: _currentSliderValue,
-                        max: 200,
-                        divisions: 20,
-                        label: _currentSliderValue.round().toString(),
+                        mouseCursor: MaterialStateMouseCursor.clickable,
+                        value: height,
+                        max: 300,
+                        divisions: 300,
+                        label: height.round().toString(),
                         onChanged: (double value) {
-                          setState(() {
-                            _currentSliderValue = value;
-                          });
+                          setState(
+                                () {
+                              height = value;
+                            },
+                          );
                         },
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      height: 160,
-                      width: 160,
-                      decoration: BoxDecoration(
-                          color: const Color(0xFF484149),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                        children: [
-                          const Text(
-                            "WEIGHT",
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
-                          const Text(
-                            "60",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Center(
-                                  child: Icon(
-                                    Icons.remove,
-                                    color: Colors.white,
-                                    size: 35,
-                                  ),
-                                ),
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all(
-                                    const Color(0xFF615664),
-                                  ),
-                                ),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Center(
-                                  child: Icon(
-                                    Icons.add,
-                                    color: Colors.white,
-                                    size: 35,
-                                  ),
-                                ),
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all(
-                                    const Color(0xFF615664),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 160,
-                      width: 160,
+                      height: MediaQuery.of(context).size.height * 0.25,
+                      width: MediaQuery.of(context).size.width * 0.4,
                       decoration: BoxDecoration(
                         color: const Color(0xFF484149),
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          const Text(
-                            "AGE",
-                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "WEIGHT",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 30),
+                              ),
+                              Text(
+                                '(kg)',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
                           ),
-                          const Text(
-                            "28",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold),
+                          Text(
+                            weight.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               IconButton(
-                                onPressed: () {},
+                                onPressed: decrementWeight,
                                 icon: const Center(
                                   child: Icon(
                                     Icons.remove,
                                     color: Colors.white,
-                                    size: 35,
+                                    size: 25,
                                   ),
                                 ),
                                 style: ButtonStyle(
@@ -231,12 +224,12 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                               IconButton(
-                                onPressed: () {},
+                                onPressed: incrementWeight,
                                 icon: const Center(
                                   child: Icon(
                                     Icons.add,
                                     color: Colors.white,
-                                    size: 35,
+                                    size: 25,
                                   ),
                                 ),
                                 style: ButtonStyle(
@@ -246,27 +239,113 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                             ],
-                          )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF484149),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text(
+                            "AGE",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          Text(
+                            age.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                onPressed: decrementAge,
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 25,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: incrementAge,
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 25,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
                 TextButton(
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.red),
-                    minimumSize:
-                        MaterialStateProperty.all(const Size(1000, 50)),
+                    backgroundColor: MaterialStateProperty.all(
+                      const Color(0xFF615664),
+                    ),
+                    minimumSize: MaterialStateProperty.all(
+                      const Size(400, 40),
+                    ),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    calculateBMI();
+                    String message;
+                    double bmi = double.parse(result);
+
+                    if (bmi <= 18.5) {
+                      message = 'Under weight';
+                    } else if (bmi <= 24.9) {
+                      message = 'Normal weight';
+                    } else {
+                      message = 'Over weight';
+                    }
+
+                    Fluttertoast.showToast(
+                        msg: message,
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.black,
+                        textColor: Colors.white,
+                        fontSize: 16.0
+                    );
+
+                  },
                   child: const Text(
                     "CALCULATE",
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
-                )
+                ),
               ],
             ),
           ),
