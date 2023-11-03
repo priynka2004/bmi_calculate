ee5d16e1a99b23b3008dec826f5f31b1ed957d6f
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2023-11-03 15:53:51 +0530

    create extensionsColorConst

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bmi_calculate/home_screen.dart';
+import 'package:bmi_calculate/screen/bmi_calculation_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const BmiCalculationScreen(),
     );
   }
 }
--- lib/screen/bmi_calculation_screen.dart
+++ lib/screen/bmi_calculation_screen.dart
@@ -317,29 +317,8 @@ class _BmiCalculationScreenState extends State<BmiCalculationScreen> {
                     ),
                   ),
                   onPressed: () {
-                    calculateBMI();
-                    String message;
-                    double bmi = double.parse(result);
-
-                    if (bmi <= 18.5) {
-                      message = StringConst.textUnderweight;
-                    } else if (bmi <= 24.9) {
-                      message = StringConst.textNormalWeight;
-                    } else {
-                      message = StringConst.textOverweight;
-                    }
-
-                    Fluttertoast.showToast(
-                        msg: message,
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.black,
-                        textColor: ColorsConst.toastTextextColor,
-                        fontSize: 16.0
-                    );
-
-                  },
+                    calculateBmi();
+                    },
                   child: const Text(
                     StringConst.textButton,
                     style: TextStyle(
@@ -353,6 +332,30 @@ class _BmiCalculationScreenState extends State<BmiCalculationScreen> {
           ),
         ),
       ),
+    );
+  }
+
+  void calculateBmi() {
+     calculateBMI();
+    String message;
+    double bmi = double.parse(result);
+
+    if (bmi <= 18.5) {
+      message = StringConst.textUnderweight;
+    } else if (bmi <= 24.9) {
+      message = StringConst.textNormalWeight;
+    } else {
+      message = StringConst.textOverweight;
+    }
+
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: ColorsConst.toastTextextColor,
+        fontSize: 16.0
     );
   }
 }
